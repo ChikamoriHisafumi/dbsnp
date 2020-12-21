6b2db50560c86dc9e8f603363aac1936b1d8f2d5
Hisafumi Chikamori <chikamori@nibiohn.go.jp>
2020-12-21 23:31:29 +0900

    qsub common sh

--- 616_qsub.sh
+++ 616_qsub.sh
@@ -6,8 +6,4 @@
 #cd {DIRECTORY}
 #############################
 
-cd /home/nibiohnproj9/chikamori/dbsnp/
-
-#############################
-
-sh 101_modified_ver.sh /home/nibiohnproj9/chikamori/dbsnp/BZ2/refsnp-chr16.json.bz2 10000000
+sh /home/nibiohnproj9/chikamori/dbsnp/600_qsub.sh 16
--- 622_qsub.sh
+++ 622_qsub.sh
@@ -6,8 +6,13 @@
 #cd {DIRECTORY}
 #############################
 
-cd /home/nibiohnproj9/chikamori/dbsnp/
-sh 101_modified_ver.sh /home/nibiohnproj9/chikamori/dbsnp/BZ2/refsnp-chr22.json.bz2 10000000
+JOB_NAME=dbsnp_chr22
+DATESTR=`date +%Y%m%d-%H%M%S`
+logfile=log_${DATESTR}_qsub_${JOB_NAME}.log
+CUR_DIR=/home/nibiohnproj9/chikamori/dbsnp
+
+cd ${CUR_DIR}/
+sh 101_modified_ver.sh ${CUR_DIR}/BZ2/refsnp-chr22.json.bz2 10000000 &> ${CUR_DIR}/LOG/${logfile}
 
 #############################
 
