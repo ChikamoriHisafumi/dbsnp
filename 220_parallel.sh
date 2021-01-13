9db97fc999493881207ff1327857bb7c4de6c0ea
WebAdmin <mizuguchilab@gmail.com>
2021-01-14 02:21:05 +0900

    new shell 220 mod.

--- 220_parallel.sh
+++ 220_parallel.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+SECONDS=0
+
 . ./settings.txt
 . ./002_constant.txt
 
@@ -6,7 +9,7 @@ DATESTR=`date +%Y%m%d-%H%M%S`
 
 PATH=${ADDITIONAL_PATH}:$PATH
 INPUT_PATH=$1
-INPUT=`basename ${INPUT_PATH}`
+FILE=`basename ${INPUT_PATH}`
 
 FILE_TYPE=`file ${INPUT_PATH}`
 
@@ -27,7 +30,7 @@ fi
 
 cd ${TEMP_DIR}
 
-split temp_process.json -l10000 --verbose ${INPUT}.
+split temp_process.json -l10000 --verbose ${FILE}.
 
 #FILE_PATH=$1
 #FILE=`basename ${FILE_PATH}`
@@ -39,13 +42,15 @@ rm -rf temp_process.json
 
 # ls -ld X_split/ > list
 
-ls -d `pwd`/${DIR}/* > list
+#ls -d `pwd`/${DIR}/* > list
+ls -d `pwd`/* > ../temp_list
+cd ..
 
-method=2
+method=$2
 
 if [ ${method} = 1 ]; then
 
-cat list | while read filepath
+cat temp_list | while read filepath
 #for filepath in ${files}; do
 do
   sh 100_json_parser.sh ${filepath}
@@ -56,12 +61,15 @@ done
 elif  [ ${method} = 2 ]; then
 
   echo 'method 2'
-  parallel -a list sh 100_json_parser.sh $1
+#  parallel -a temp_list sh 100_json_parser.sh $1
+  parallel sh 100_json_parser.sh < temp_list 
+  echo 'parallel!'
 
 fi
 
-PRODUCT_DIR=product_${DATESTR}
+PRODUCT_DIR=product_${FILE}_${DATESTR}
 
+mkdir TABLE
 mkdir TABLE/${PRODUCT_DIR}
 
 cat TABLE/table1_${FILE}*.tsv > TABLE/table1_${FILE}_tsv
@@ -76,5 +84,13 @@ cat TABLE/table3_${FILE}*.tsv > TABLE/table3_${FILE}_tsv
 rm -rf TABLE/table3_${FILE}*.tsv
 mv TABLE/table3_${FILE}_tsv TABLE/${PRODUCT_DIR}/table3_${FILE}.tsv
 
+rm -rf TEMP_${DATESTR}
+rm -rf temp_list
 
+sleep 3
+
+time_1=$SECONDS
+log_1='It took '${time_1}' seconds to generate temporary formatted files.'
+
+echo ${log_1}
 
