ca3ed1dbab2114fd6b45541688310f28ab2aa6d1
WebAdmin <mizuguchilab@gmail.com>
2021-01-14 05:37:21 +0900

    modified 000_first.sh

--- 000_first.sh
+++ 000_first.sh
@@ -1,7 +1,25 @@
 cat << EOH > settings.txt
+#
+# IMPORTANT (if you run this script on cron or qsub, please read this section carefully)
+# Please input additional path if you install 'jq' and 'parallel' without yum and so on.
+# For example, the FULL path of these bin files are catenated with ':'.
+#
+# ADDITIONAL_PATH=/home/chikamori/bin
+#
+# or
+#
+# ADDITIONAL_PATH=/home/chikamori/bin:/home/chikamori/bin/parallel-20201222/src
+#
 
-# Please input additional path if you install jq without yum and so on.
 ADDITIONAL_PATH=
+
+# Please input top directory's full path of local repository cloned from GitHub.
+# For example, 
+# DBSNP_PATH=/home/chikamori/dbsnp/
+
+DBSNP_PATH=
+
+# These variable is not so important. Please let it be.
 
 DEBUG_MODE_01=no
 DEBUG_MODE_02=yes
